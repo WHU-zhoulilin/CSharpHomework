af219eaf13a3d22d4b36786f682427d4c62bdfd9
zhoulilin <779329434@qq.com>
2018-09-16 11:42:35 +0800

    homework1

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=0, y=0;
+            try
+            {
+                Console.WriteLine("请输入第一个乘数：");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("请输入第二个乘数：");
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(x + " x " + y + " = " + (x * y));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            try
+            {
+                x = Convert.ToDouble(textBox1.Text);
+                y = Convert.ToDouble(textBox2.Text);
+                label3.Text = ("" + (x * y));
+            }
+            catch
+            {
+                MessageBox.Show("你的输入有误！");
+            }
+        }
+    }
+}
